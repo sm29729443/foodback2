10385a500e705a94056cec4ff5adc9ad606b954f
Fish <sm29729443@gmail.com>
2023-12-19 12:58:42 +0800

    tong1

--- src/main/java/com/twoTeam/groupProject/GroupProjectApplication.java
+++ src/main/java/com/twoTeam/groupProject/GroupProjectApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class GroupProjectApplication {
-	// one branch 測試
-	// two branch 測試
+	// 測試
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroupProjectApplication.class, args);
