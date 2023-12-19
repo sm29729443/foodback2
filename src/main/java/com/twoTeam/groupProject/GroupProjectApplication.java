0892da1be9221b765ccd615e3f7ea65bc2c726d9
Fish <sm29729443@gmail.com>
2023-12-19 12:57:39 +0800

    initial

---
+++ src/main/java/com/twoTeam/groupProject/GroupProjectApplication.java
@@ -0,0 +1,18 @@
+package com.twoTeam.groupProject;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class GroupProjectApplication {
+	// one branch 測試
+	// two branch 測試
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProjectApplication.class, args);
+	}
+
+}
